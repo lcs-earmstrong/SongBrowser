920b796585d09224bfdbe0fbab1575a3e4a70275
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-22 11:15:04 -0500

    Have made it so that an artist name will pop up but it still won't play a song

--- SongBrowser/Views/ContentView.swift
+++ SongBrowser/Views/ContentView.swift
@@ -44,7 +44,8 @@ struct ContentView: View {
         List(songs, id: \.trackId){currentSong in
         
             
-            VStack(alignment .leading){
+            VStack(alignment: .leading){
+                
                 
                 Text(currentSong.trackName)
             
@@ -62,9 +63,14 @@ struct ContentView: View {
     
     //Mark: FUnctions
     func fetchSongResults() {
+    
+        
+        // sanitize the search input
+        
+        let input = searchText.lowercased().replacingOccurrences(of: " ", with: "+")
         
         // Set the address of the JSON endpoint
-        let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")!
+        let url = URL(string: "https://itunes.apple.com/search?term=\(input)&entity=song")!
 
         // Configure a URLRequest instance
         // Defines what type of request will be sent to the address noted above
